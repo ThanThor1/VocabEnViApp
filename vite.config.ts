3cd4ba89f6418385e2f6ffee478c06525122087c
ThanThor1 <vuxuandungnb2k5@gmail.com>
2026-01-17 22:10:08 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import electron from 'vite-plugin-electron/simple'
 
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+    electron({
+      main: {
+        entry: 'electron/main.js'
+      },
+      preload: {
+        input: 'electron/preload.js'
+      }
+    })
+  ],
   server: {
-    port: 5173
+    port: 5173,
+    host: '127.0.0.1'
   }
 })
